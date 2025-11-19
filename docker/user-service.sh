cd8447585f1886da9c738b2aae58f69166af7d7d
nikit <nikitatayatsko308@gmail.com>
2025-11-19 23:33:44 +0300

    final
    # Conflicts:
    #	docker/Dockerfile

--- docker/user-service.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-PROFILE=${PROFILE:-local-idea}
-
-echo "Starting service with profile: ${PROFILE}"
-exec java -jar /srv/user-service-0.0.1-SNAPSHOT.jar --spring.profiles.active=$PROFILE
