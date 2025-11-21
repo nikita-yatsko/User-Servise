0fb8064901a4442190e759fb5975676a8e16f8a7
nikit <nikitatayatsko308@gmail.com>
2025-11-21 15:26:03 +0300

    final v1.0 working

--- src/main/java/com/userservise/app/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/userservise/app/security/filter/JwtAuthenticationFilter.java
@@ -1,12 +1,15 @@
 package com.userservise.app.security.filter;
 
+import com.userservise.app.model.constants.ErrorMessage;
 import com.userservise.app.model.dto.AuthResponse;
+import com.userservise.app.model.exception.InvalidTokenException;
 import com.userservise.app.security.model.CustomUserDetails;
 import com.userservise.app.service.Impl.AuthServiceClient;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,7 +19,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.naming.AuthenticationException;
 import java.io.IOException;
 import java.util.List;
 
@@ -29,8 +31,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         String header = request.getHeader("Authorization");
@@ -45,7 +47,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     UsernamePasswordAuthenticationToken authentication = getUsernamePasswordAuthenticationToken(auth);
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 } else {
-                    throw new AuthenticationException("Invalid token"); // TODO change message
+                    throw new InvalidTokenException(ErrorMessage.INVALID_TOKEN.getMessage());
                 }
 
             } catch (Exception e) {
